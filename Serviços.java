2062d151685baa8553845527bace19bd88043086
Luiz Henrique <luizthefisher@gmail.com>
2026-04-24 15:05:30 -0300

    Add files via upload

---
+++ Serviços.java
@@ -0,0 +1,25 @@
+package barbearia;
+
+public class Serviços {
+	
+	String tipo;
+	double valor;
+	
+	Serviços(String tipo, double valor) {
+		this.tipo = tipo;
+		this.valor = valor;
+		
+	}
+	void exibirServiço() {
+		System.out.println("===================");
+		System.out.println("Tipo: " + this.tipo);
+		System.out.println("Valor: " + this.valor);
+	
+	}
+	
+		
+	
+	
+
+}
+
